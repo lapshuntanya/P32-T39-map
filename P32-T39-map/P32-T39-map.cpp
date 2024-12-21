fb73980173e95a6a8d6a452ef8ffe5d87588540f
lapshuntanya <lapshun.tatiana@gmail.com>
2024-12-21 11:27:41 +0200

    task1

--- P32-T39-map/P32-T39-map.cpp
+++ P32-T39-map/P32-T39-map.cpp
@@ -3,8 +3,29 @@
 #include <string>
 using namespace std;
 
-int main()
-{
+int main(){
     map<string, int> words;
-    
+    string user;
+
+    do {
+        cout << "Input word: "; cin >> user;
+
+        auto item = words.find(user);
+        if (item == words.end()) {
+           // words.insert(pair<string, int>(user, 1));
+            words[user] = 1;
+        }
+        else {
+           // item->second = item->second + 1;
+            words[user]++;
+        }
+    } while (user != "stop");
+    cout << "------------------------------------\n";
+    words.erase("stop");
+    /*for (auto node : words) { cout << node.first << " " << node.second << endl;   }*/
+
+    for (auto it = words.begin(); it != words.end(); it++) {
+        cout << it->first << " " << it->second << endl;
+    }   
+    return 0;
 }
